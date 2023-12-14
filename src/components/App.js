292d61350878c00ea6345b20654501048c51f002
Rizve Rahman Reza <rizverahman.ai@gmail.com>
2023-12-14 12:07:22 +0600

    v_1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Entry from './Entry'
+
+// Import the generated emojipedia array
+import emojipediaArray from './generateEmojiEntry' // Adjust the path accordingly
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className='dictionary'>
+        {emojipediaArray.map((emojiEntry) => (
+          <Entry
+            key={emojiEntry.id}
+            emoji={emojiEntry.emoji}
+            name={emojiEntry.name}
+            meaning={emojiEntry.meaning}
+          />
+        ))}
+      </dl>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/generateEmojiEntry.js
@@ -0,0 +1,60 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: '💪',
+      name: 'Tense Biceps',
+      meaning: '“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.',
+    },
+    {
+      id: 2,
+      emoji: '🙏',
+      name: 'Person With Folded Hands',
+      meaning: 'Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.',
+    },
+    {
+      id: 3,
+      emoji: '🤣',
+      name: 'Rolling On The Floor, Laughing',
+      meaning: 'This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.',
+    },
+    {
+      id: 4,
+      emoji: '😌',
+      name: 'Relieved Face',
+      meaning: 'Everything went well! The relieved face 😌 radiates a calming atmosphere. It is a gentle smile that signals that an unpleasant situation has been successfully handled.',
+    },
+    {
+      id: 5,
+      emoji: '😎',
+      name: 'Cool Face',
+      meaning: 'Feeling cool and confident. Wearing sunglasses with a smile.',
+    },
+    {
+      id: 6,
+      emoji: '🌟',
+      name: 'Shining Star',
+      meaning: 'A shining star. Represents achievement, excellence, and success.',
+    },
+    {
+      id: 7,
+      emoji: '🚀',
+      name: 'Rocket',
+      meaning: 'A rocket taking off. Symbolizes speed, progress, and excitement.',
+    },
+    {
+      id: 8,
+      emoji: '🌈',
+      name: 'Rainbow',
+      meaning: 'A colorful rainbow. Represents diversity, hope, and positivity.',
+    },
+    {
+      id: 9,
+      emoji: '🎉',
+      name: 'Celebration',
+      meaning: 'Celebrating something special. Can be used for birthdays, parties, or any joyous occasion.',
+    },
+    // Continue adding entries up to 100...
+  ];
+  
+  export default emojipedia;
+  
